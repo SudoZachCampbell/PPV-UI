9fd165e2e871592d18df2f03cbb2021bf00b95e6
40132243 <zcampbell03@qub.ac.uk>
2019-11-13 19:05:47 +0000

    Some small changes

---
+++ src/containers/PropertyList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PropertyList = (props) => {
+    return (
+        <div>
+            Magic Hello to you {props.name};
+        </div>
+    );
+};
+
+export default PropertyList;
