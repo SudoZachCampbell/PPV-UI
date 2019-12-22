0d09df772d3aee01e73cc3f69b4f014845090502
40132243 <zcam1995@gmail.com>
2019-12-22 21:40:57 +0000

    Moved SideBar

--- src/components/FilterToggle/FilterToggle.jsx
+++ src/components/FilterToggle/FilterToggle.jsx
@@ -4,16 +4,29 @@ import Button from '@material-ui/core/Button';
 import DoubleArrow from '@material-ui/icons/DoubleArrow';
 import Checkbox from '@material-ui/core/Checkbox';
 import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box'
+import Box from '@material-ui/core/Box';
+
+import { makeStyles } from '@material-ui/core/styles';
 
 import './FilterToggle.scss';
+import { orange } from '@material-ui/core/colors';
+
+const useStyles = makeStyles({
+  box: {
+    width: '6.5vw',
+    height: '6.5vw',
+    backgroundColor: 'orange'
+  }
+});
 
 export default function FilterToggle(props) {
   const [toggled, setToggled] = useState(false);
   const [apply, setApply] = useState(false);
 
+  const classes = useStyles();
+
   const toggleChange = () => {
-    (!apply && !toggled) && setApply(true); 
+    !apply && !toggled && setApply(true);
     props.callback(!toggled ? 1 : -1);
     setToggled(!toggled);
   };
@@ -23,12 +36,26 @@ export default function FilterToggle(props) {
   };
 
   return (
-    <Box display='flex' flexDirection='row' className={props.outerClass ? props.outerClass + ' box-index' : 'box-index'}>
-      <Checkbox checked={apply} onChange={applyChanged} />
-      <Button onClick={toggleChange}>
-        <DoubleArrow color={toggled ? 'primary' : 'secondary'} />
-      </Button>
-      {toggled ? props.children : <Typography style={{display:'inline'}}>{props.children.props.label}</Typography>}
+    <Box
+      display='flex'
+      flexDirection='row'
+      className={
+        props.outerClass ? props.outerClass + ' box-index' : 'box-index'
+      }
+    >
+      <div className={classes.box}>
+        <Checkbox checked={apply} onChange={applyChanged} />
+        <Button onClick={toggleChange}>
+          <DoubleArrow color={toggled ? 'primary' : 'secondary'} />
+        </Button>
+      </div>
+      {toggled ? (
+        props.children
+      ) : (
+        <Typography style={{ display: 'inline' }}>
+          {props.children.props.label}
+        </Typography>
+      )}
     </Box>
   );
 }
