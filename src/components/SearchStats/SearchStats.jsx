1f853dadf3cfc5c21845e32eed517c76c26fb088
40132243 <zcam1995@gmail.com>
2020-01-12 16:25:56 +0000

    Added Rent BarGraph, Started AreaGraph

--- src/components/SearchStats/SearchStats.jsx
+++ src/components/SearchStats/SearchStats.jsx
@@ -1,19 +1,21 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
-import BarGraph from '../BarGraph/BarGraph' 
+import BarGraph from '../BarGraph/BarGraph';
 import { Typography } from '@material-ui/core';
 
 export default function SearchStats(props) {
-    const [keywords, setKeywords] = useState({});
+  const [keywords, setKeywords] = useState({});
 
+  useEffect(() => {}, [props.result]);
 
-      useEffect(() => {
-
-      }, [props.result]);
-
-    return(
-        <div>
-            <BarGraph data={props.result.keywords} x='keyword' y='count'/>
-        </div>
-    )
+  return (
+    <div>
+      <div>
+        <BarGraph data={props.result.keywords} x='keyword' y='count' />
+      </div>
+      <div>
+        <BarGraph data={props.result.priceCounts} x='price' y='count' />
+      </div>
+    </div>
+  );
 }
